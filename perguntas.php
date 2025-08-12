7261f9e1be30ac8cd46bdc372a681835d5068d7c
Kevin luan <cardosoluan3006@gmail.com>
2025-08-12 16:36:52 -0300

    Tocando cores da tela de perguntas

--- perguntas.php
+++ perguntas.php
@@ -123,7 +123,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>selecct enderecos</title>
-    <link rel="stylesheet" href="perguntas.css">
+    <link rel="stylesheet" href="pergunta.css">
 
 
 </head>
